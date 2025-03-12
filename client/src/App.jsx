eb58a1d7d06c3548deb5d85e98e7e6548538d495
Krasimir Petranov <kimim0548@gmail.com>
2025-03-12 17:24:19 +0000

    extract main components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './components/home/Home'
+import Header from './components/header/Header'
 
 export default function App() {
 
   return (
 	<div id="box">
-	
+		<Header />
+
+		<main id="main-content">
+			<Home />
+        </main>
+
 	</div>
   )
 }
